a003086cbba8ce8cf68135b8a0582869322e5d7a
Fleming <47439078+Fleming@users.noreply.github.com>
2021-04-14 23:24:54 -0400

    changed api key

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "http://maps.openweathermap.org/maps/2.0/weather/{op}/{z}/{x}/{y}&appid={API key}"
+weather_api_key = "http://api.openweathermap.org/data/2.5/weather?"
 
 # Google API Key
 g_key = "YOUR KEY HERE!"
