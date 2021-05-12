edad8e8c0d52ec62a0b0b803df01c3e0c9e5b2a5
Fleming <47439078+Fleming@users.noreply.github.com>
2021-05-12 17:57:56 -0400

    replaced invalid api key

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,8 +1,9 @@
 # OpenWeatherMap API Key
-weather_api_key = "http://api:api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+weather_api_key = "api:api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB8IAMrnI074vA0XH2AH1Kj1lrvkIWnsUc"
 
 
 # try these api:api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key} , http://api.openweathermap.org/data/2.5/weather?
+# http://api:api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
