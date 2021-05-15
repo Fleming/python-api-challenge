a486fbd7ef44c42a9d4385ef7a951006d1e1279e
Fleming <47439078+Fleming@users.noreply.github.com>
2021-05-15 15:04:17 -0400

    fixed api(for real this time)

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "api:api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+weather_api_key = "7e95c280e82e44c119e6f129ea764f93"
 
 # Google API Key
 g_key = "AIzaSyB8IAMrnI074vA0XH2AH1Kj1lrvkIWnsUc"
